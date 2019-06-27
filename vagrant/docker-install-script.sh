8fef57ddd8ee239f9ad2e2f5c208906b3c290679
David Caballero <david.caballero@qvantel.com>
2019-06-27 15:52:43 -0500

    add docker compose install for vagrant vm

--- vagrant/docker-install-script.sh
+++ vagrant/docker-install-script.sh
@@ -25,4 +25,8 @@ sudo docker run --rm hello-world
 # install docker-compose steps
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo docker-compose -version
